215dd6dbbe531733f7ae320d75b25a6f59978fd3
Jeff Newman <jeff@driftless.xyz>
2024-08-28 10:10:32 -0500

    update info message with license

--- passengersim/cli/info.py
+++ passengersim/cli/info.py
@@ -1,4 +1,4 @@
-from passengersim_core import build_expiration
+from passengersim_core import build_expiration, license_info
 from passengersim_core._version import __version__ as __core_version__
 from rich import print
 
@@ -11,11 +11,13 @@ def info():
     print(
         f"""\
 :seat: [bold dark_goldenrod]PassengerSim[/bold dark_goldenrod]
-  [black not bold]Version {__version__}
-  Core Version {__core_version__}
-  Copyright (c) 2023
-  This in-development tool is not for public distribution.
-  [red]This build expires {build_expiration().astimezone()}[/red]
-  [link=https://www.passengersim.com/]https://www.passengersim.com[/link]
+   [black not bold]Version {__version__}
+   Core Version {__core_version__}
+   Copyright (c) 2024 PassengerSim LLC
+   This in-development tool is not for public distribution.
+   [red]This build expires {build_expiration().astimezone()}[/red]
 """
     )
+    for i in license_info():
+        print(f"  [magenta]{i}[/magenta]")
+    print("  [link=https://www.passengersim.com/]https://www.passengersim.com[/link]")

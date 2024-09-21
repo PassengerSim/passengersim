4b69bc1fd3edfeda24a76ab043aad8f25af5e873
Jeff Newman <jeff@driftless.xyz>
2024-09-20 19:52:46 -0500

    docstrings

--- passengersim/summaries/__init__.py
+++ passengersim/summaries/__init__.py
@@ -1,3 +1,18 @@
+"""
+This subpackage is for defining the SimulationTables class and its items.
+
+The SimulationTables class is a container for summary tables and figures
+that are extracted from a Simulation object after it has been run.
+
+Each item in the SimulationTables class is defined by a SimulationTableItem
+instance, which specifies how to extract and aggregate the summary data.
+Each of these is defined in a separate module in this subpackage.  Within
+each module, the SimulationTableItem instance is created using the
+`.generic.SimulationTable_add_item()` function.
+"""
+
+from __future__ import annotations
+
 from collections.abc import Callable
 
 import pandas as pd
@@ -11,6 +26,14 @@ from .segmentation_by_timeframe import (
 
 
 class SimulationTables(_GenericSimulationTables):
+    """Container for summary tables and figures extracted from a Simulation.
+
+    This class is a subclass of _GenericSimulationTables, which is defined in
+    the generic module.  It lists the items that are available in the
+    SimulationTables class, and provides type hints and (optionally, but
+    ideally) documentation for the data that is stored in each item.
+    """
+
     segmentation_by_timeframe: pd.DataFrame
     legs: pd.DataFrame
 

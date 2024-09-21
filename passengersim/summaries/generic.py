3868f663fbfff00430c83c85540431878a66c3cf
Jeff Newman <jeff@driftless.xyz>
2024-09-21 11:05:19 -0500

    pickles

--- passengersim/summaries/generic.py
+++ passengersim/summaries/generic.py
@@ -1,5 +1,10 @@
 from __future__ import annotations
 
+import glob
+import os
+import pathlib
+import pickle
+import time
 import warnings
 from collections.abc import Callable, Collection
 from typing import TYPE_CHECKING, Any
@@ -74,8 +79,6 @@ class SimulationTableItem:
 
 
 class _GenericSimulationTables:
-    __slots__ = ("_data", "config", "cnx", "sim", "n_total_samples", "meta_summaries")
-
     def __init__(
         self,
         data: dict[str, pd.DataFrame] = None,
@@ -105,6 +108,25 @@ class _GenericSimulationTables:
 
         self.meta_summaries = []
         """Summaries that were aggregated to create this summary."""
+
+    __writable_attrs = {
+        "_data",
+        "config",
+        "cnx",
+        "sim",
+        "n_total_samples",
+        "meta_summaries",
+        "_preserve_meta_summaries",
+        "_preserve_config",
+    }
+
+    def __setattr__(self, item, value):
+        """Deny setting of attributes that are not in __writable_attrs."""
+        if item in self.__writable_attrs:
+            # writable attributes are handled normally
+            super().__setattr__(item, value)
+        else:
+            raise AttributeError(f"Cannot set attribute {item!r}")
 
     _std_agg: dict[
         str, Callable[[list[_GenericSimulationTables]], pd.DataFrame | None]
@@ -149,6 +171,130 @@ class _GenericSimulationTables:
         result.meta_summaries = summaries
         result.n_total_samples = sum(s.n_total_samples for s in summaries)
         return result
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        if "cnx" in state:
+            del state["cnx"]
+        if "sim" in state:
+            del state["sim"]
+        if "config" in state:
+            if state.get("_preserve_config", False):
+                state["_config_yaml"] = state["config"].to_yaml()
+            del state["config"]
+        if "meta_trials" in state and not state.get("_preserve_meta_trials", True):
+            del state["meta_trials"]
+        if "_preserve_meta_trials" in state:
+            del state["_preserve_meta_trials"]
+        if "_preserve_config" in state:
+            del state["_preserve_config"]
+        return state
+
+    def __setstate__(self, state):
+        if "_config_yaml" in state:
+            state["config"] = Config.from_raw_yaml(state.pop("_config_yaml"))
+        self.__dict__.update(state)
+
+    def to_pickle(
+        self,
+        filename: str | pathlib.Path,
+        add_timestamp_ext: bool = True,
+        *,
+        preserve_meta_summaries: bool = False,
+        preserve_config: bool = False,
+    ):
+        """Save to a pickle file.
+
+        This method uses lz4 compression if the lz4.frame module is available.
+
+        Parameters
+        ----------
+        filename : str or Path-like
+            The filename to save the object to.  An extension map be added or
+            modified, to optionally add a time stamp and/or compression flag.
+        add_timestamp_ext : bool, default True
+            Add a timestamp extension to the filename.
+        preserve_meta_summaries : bool, default False
+            Preserve the meta_summaries attribute in the saved object.
+        preserve_config : bool, default False
+            Preserve the config attribute in the saved object.  This includes
+            the entire network, and can potentially be a lot of data.
+        """
+        if add_timestamp_ext:
+            filename = pathlib.Path(filename)
+            timestamp = time.strftime("%Y%m%d-%H%M%S")
+            filename = filename.with_suffix(f".{timestamp}.pkl")
+        else:
+            filename = pathlib.Path(filename)
+
+        try:
+            import lz4.frame
+        except ImportError:
+            with open(filename, "wb") as f:
+                self._preserve_meta_summaries = preserve_meta_summaries
+                self._preserve_config = preserve_config
+                pickle.dump(self, f)
+                del self._preserve_meta_summaries
+                del self._preserve_config
+        else:
+            with lz4.frame.open(
+                filename.with_suffix(filename.suffix + ".lz4"), "wb"
+            ) as f:
+                self._preserve_meta_summaries = preserve_meta_summaries
+                self._preserve_config = preserve_config
+                pickle.dump(self, f)
+                del self._preserve_meta_summaries
+                del self._preserve_config
+
+    @classmethod
+    def from_pickle(cls, filename: str | pathlib.Path, read_latest: bool = True):
+        """Load the object from a pickle file.
+
+        Parameters
+        ----------
+        filename : str or Path-like
+            The filename to load the object from.
+        read_latest : bool, default True
+            If True, read the latest file matching the pattern.
+        """
+        try:
+            import lz4.frame
+        except ImportError:
+            pass
+        else:
+            # first try lz4 compressed files if available
+            try:
+                if read_latest:
+                    filename_glob = pathlib.Path(filename).with_suffix(".*.pkl.lz4")
+                    files = sorted(glob.glob(str(filename_glob)))
+                    if not files:
+                        if not os.path.exists(filename):
+                            raise FileNotFoundError(filename)
+                    else:
+                        filename = files[-1]
+
+                with lz4.frame.open(filename, "rb") as f:
+                    result = pickle.load(f)
+                    if result.__class__.__name__ != cls.__name__:
+                        raise TypeError(f"Expected {cls}, got {type(result)}")
+                    return result
+            except FileNotFoundError:
+                pass
+
+        if read_latest:
+            filename_glob = pathlib.Path(filename).with_suffix(".*.pkl")
+            files = sorted(glob.glob(str(filename_glob)))
+            if not files:
+                if not os.path.exists(filename):
+                    raise FileNotFoundError(filename)
+            else:
+                filename = files[-1]
+
+        with open(filename, "rb") as f:
+            result = pickle.load(f)
+            if result.__class__.__name__ != cls.__name__:
+                raise TypeError(f"Expected {cls}, got {type(result)}")
+            return result
 
 
 def SimulationTable_add_item(name: str, *args, **kwargs):

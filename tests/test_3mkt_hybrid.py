cd3e01f03044a6038d3bfb24a295cdccff3c1b99
Jeff Newman <jeff@driftless.xyz>
2024-10-22 21:29:41 -0500

    update tests for EM

--- tests/test_3mkt_hybrid.py
+++ tests/test_3mkt_hybrid.py
@@ -19,26 +19,44 @@ def config() -> Config:
     return cfg
 
 
-@pytest.fixture(scope="module")
-def summary(config: Config) -> SimulationTables:
+@pytest.fixture(scope="module", params=[None, "em"])
+def summary(
+    config: Config, request: pytest.FixtureRequest
+) -> (str | None, SimulationTables):
+    detruncation = request.param
+    config.rm_systems.rm_hybrid.processes.dcp.forecast.detruncation_algorithm = (
+        detruncation
+    )
     sim = Simulation(config)
-    return sim.run(summarizer=SimulationTables)
+    return detruncation, sim.run(summarizer=SimulationTables)
 
 
-@pytest.fixture(scope="module")
-def summary_mp(config: Config) -> SimulationTables:
+@pytest.fixture(scope="module", params=[None, "em"])
+def summary_mp(
+    config: Config, request: pytest.FixtureRequest
+) -> (str | None, SimulationTables):
+    detruncation = request.param
+    config.rm_systems.rm_hybrid.processes.dcp.forecast.detruncation_algorithm = (
+        detruncation
+    )
     sim = MultiSimulation(config)
-    return sim.run(summarizer=SimulationTables)
+    return detruncation, sim.run(summarizer=SimulationTables)
 
 
-@pytest.fixture(scope="module", params=["ki", "mr"])
+@pytest.fixture(
+    scope="module", params=[("ki", None), ("mr", None), ("ki", "em"), ("mr", "em")]
+)
 def summary_fare_adjustment(
     config: Config, request: pytest.FixtureRequest
 ) -> (str, SimulationTables):
-    fare_adj = request.param
+    fare_adj = request.param[0]
+    detruncation = request.param[1]
     config.rm_systems.rm_hybrid.processes.dcp.forecast.fare_adjustment = fare_adj
+    config.rm_systems.rm_hybrid.processes.dcp.forecast.detruncation_algorithm = (
+        detruncation
+    )
     sim = Simulation(config)
-    return fare_adj, sim.run(summarizer=SimulationTables)
+    return fare_adj, detruncation, sim.run(summarizer=SimulationTables)
 
 
 TABLES = [
@@ -55,15 +73,25 @@ TABLES = [
     "path_forecasts",
 ]
 
+TABLE_NOT_SENSITIVE_TO_RM = [
+    "demand_to_come",
+    "demand_to_come_summary",
+]
+
 
 @pytest.mark.parametrize("table_name", TABLES)
 def test_3mkt_hybrid_table_single_process(
     summary, dataframe_regression, table_name: str
 ):
+    detruncation, summary = summary
     assert isinstance(summary, SimulationTables)
     df = getattr(summary, table_name)
+    if table_name in TABLE_NOT_SENSITIVE_TO_RM:
+        basename = table_name
+    else:
+        basename = f"{detruncation}-{table_name}"
     dataframe_regression.check(
-        df, basename=table_name, default_tolerance=DEFAULT_TOLERANCE
+        df, basename=basename, default_tolerance=DEFAULT_TOLERANCE
     )
 
 
@@ -71,10 +99,15 @@ def test_3mkt_hybrid_table_single_process(
 def test_3mkt_hybrid_table_multi_process(
     summary_mp, dataframe_regression, table_name: str
 ):
+    detruncation, summary_mp = summary_mp
     assert isinstance(summary_mp, SimulationTables)
     df = getattr(summary_mp, table_name)
+    if table_name in TABLE_NOT_SENSITIVE_TO_RM:
+        basename = table_name
+    else:
+        basename = f"{detruncation}-{table_name}"
     dataframe_regression.check(
-        df, basename=table_name, default_tolerance=DEFAULT_TOLERANCE
+        df, basename=basename, default_tolerance=DEFAULT_TOLERANCE
     )
 
 
@@ -82,9 +115,13 @@ def test_3mkt_hybrid_table_multi_process(
 def test_3mkt_hybrid_fareadj_table_single_process(
     summary_fare_adjustment, dataframe_regression, table_name: str
 ):
-    fare_adj, run_summary = summary_fare_adjustment
+    fare_adj, detruncation, run_summary = summary_fare_adjustment
     assert isinstance(run_summary, SimulationTables)
     df = getattr(run_summary, table_name)
+    if table_name in TABLE_NOT_SENSITIVE_TO_RM:
+        basename = table_name
+    else:
+        basename = f"{fare_adj}-{detruncation}-{table_name}"
     dataframe_regression.check(
-        df, basename=f"{fare_adj}-{table_name}", default_tolerance=DEFAULT_TOLERANCE
+        df, basename=basename, default_tolerance=DEFAULT_TOLERANCE
     )

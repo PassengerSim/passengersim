cedd1268f8e104ad45a17fc0dd1c2dfd61b3ae8a
Jeff Newman <jeff@driftless.xyz>
2025-01-13 10:15:22 -0600

    don't rebuild notebooks if no changes

--- docs/.scripts/_execute_notebooks.py
+++ docs/.scripts/_execute_notebooks.py
@@ -1,5 +1,7 @@
 """Execute notebooks."""
 
+import hashlib
+import os.path
 import re
 import shutil
 from pathlib import Path
@@ -30,15 +32,39 @@ for path in sorted(doc_path.rglob("*.ipynb")):
     if exclude:
         continue
 
-    print(f"converting {path} to {path.with_suffix('.nbconvert.ipynb')}")
+    print(f"📓 reading {path} to {path.with_suffix('.nbconvert.ipynb')}")
+    with open(path, "rb") as f:
+        content = f.read()
+        hash_value = hashlib.sha256(content).hexdigest()
+        print(f"  SHA-256 hash of the notebook content: {hash_value}")
+
+    skip_execution = False
+    if os.path.exists(path.with_suffix(".hash.txt")):
+        with open(path.with_suffix(".hash.txt"), encoding="utf-8") as f:
+            if f.read() == hash_value:
+                print("  🌟 The notebook content has not changed. Skipping execution.")
+                skip_execution = True
+            else:
+                print("  💥 The notebook content has changed. Re-executing.")
+                os.remove(path.with_suffix(".hash.txt"))
+
+    if skip_execution:
+        continue
+
     with open(path) as f:
         nb = nbformat.read(f, as_version=4)
         ep = ExecutePreprocessor(timeout=600)
+        clean_execution = True
         try:
             ep.preprocess(nb, {"metadata": {"path": path.parent}})
         except CellExecutionError as err:
             print(f'Error executing the notebook "{path}".')
             print(f"\033[1;32m{err}\033[0m")
+            clean_execution = False
         finally:
+            print(f"  writing to {path.with_suffix('.nbconvert.ipynb')}")
             with open(path.with_suffix(".nbconvert.ipynb"), "w", encoding="utf-8") as f:
                 nbformat.write(nb, f)
+            if clean_execution:
+                with open(path.with_suffix(".hash.txt"), "w", encoding="utf-8") as f:
+                    f.write(hash_value)

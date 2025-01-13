fdf7d2c22d78749a9a744ddfc65cef17b77289af
Jeff Newman <jeff@driftless.xyz>
2025-01-13 15:37:04 -0600

    allow pipe sep restrictions

--- passengersim/config/fares.py
+++ passengersim/config/fares.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import re
+
 from pydantic import BaseModel, field_validator
 
 
@@ -19,4 +21,11 @@ class Fare(BaseModel, extra="forbid"):
         """Allow restrictions to be None or missing."""
         if v is None:
             v = []
+        return v
+
+    @field_validator("restrictions", mode="before")
+    def allow_pipe_sep(cls, v):
+        """Allow restrictions to be a string of pipe or slash separated values."""
+        if isinstance(v, str):
+            v = list(filter(None, re.split(r"[|/]", v)))
         return v

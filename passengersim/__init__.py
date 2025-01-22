0ff6af32918b58b5bf694f4170fa6ca7645c82ff
Jeff Newman <jeff@driftless.xyz>
2025-01-22 15:21:07 -0600

    foce lowercase

--- passengersim/__init__.py
+++ passengersim/__init__.py
@@ -32,7 +32,14 @@ def demo_network(name: str):
 
     if not name.endswith(".yaml"):
         name = f"{name}.yaml"
-    return importlib.resources.files(__package__).joinpath("networks").joinpath(name)
+    # note: on macOS by default the file system is not case sensitive, but
+    # on Linux it is. All the network files are lowercase, so we can get around
+    # this by forcing the name to lowercase.
+    return (
+        importlib.resources.files(__package__)
+        .joinpath("networks")
+        .joinpath(name.lower())
+    )
 
 
 def versions(verbose=False):

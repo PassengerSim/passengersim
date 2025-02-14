efad0280f498c877c0561f42f390d86c6b7145d5
Jeff Newman <jeff@driftless.xyz>
2025-02-14 14:43:55 -0600

    trace bid price std_dev

--- passengersim/tracers/bid_price.py
+++ passengersim/tracers/bid_price.py
@@ -41,7 +41,7 @@ class LegBidPriceTracer(GenericTracer):
             )
 
     def finalize(self) -> pd.DataFrame:
-        return pd.DataFrame(
+        m = pd.DataFrame(
             {
                 leg_id: {
                     days_prior: welford.mean for days_prior, welford in welfords.items()
@@ -49,9 +49,21 @@ class LegBidPriceTracer(GenericTracer):
                 for leg_id, welfords in self.bid_prices.items()
             }
         ).rename_axis(columns="leg_id", index="days_prior")
+        s = pd.DataFrame(
+            {
+                leg_id: {
+                    days_prior: welford.sample_std_dev
+                    for days_prior, welford in welfords.items()
+                }
+                for leg_id, welfords in self.bid_prices.items()
+            }
+        ).rename_axis(columns="leg_id", index="days_prior")
+        return pd.concat([m, s], keys=["mean", "std_dev"], names=["statistic"])
 
 
-def fig_leg_bid_prices(summary: SimulationTables, *, raw_df: bool = False) -> alt.Chart:
+def fig_leg_bid_prices(
+    summary: SimulationTables, *, std_dev: bool = True, raw_df: bool = False
+) -> alt.Chart:
     from passengersim.contrast import Contrast  # prevent circular import
 
     if isinstance(summary, Contrast):
@@ -59,23 +71,29 @@ def fig_leg_bid_prices(summary: SimulationTables, *, raw_df: bool = False) -> al
         df = pd.concat(dfs, names=["source"]).reset_index()
     else:
         df = summary.callback_data.leg_bid_prices
-        df = df.stack().rename("bid_price").reset_index()
+        df = df.stack().unstack("statistic").reset_index()
 
     if raw_df:
         return df
 
     import altair as alt
 
-    fig = (
-        alt.Chart(df)
-        .mark_line()
-        .encode(
-            x=alt.X("days_prior:Q").scale(reverse=True),
-            y=alt.Y("bid_price:Q"),
-            color="leg_id:N",
-        )
+    chart = alt.Chart(df)
+    fig = chart.mark_line().encode(
+        x=alt.X("days_prior:Q").scale(reverse=True),
+        y=alt.Y("mean:Q", title="Mean Bid Price"),
+        color="leg_id:N",
     )
     if "source" in df.columns:
         fig = fig.encode(strokeDash="source:N", strokeWidth="source:N")
+    if std_dev:
+        fig1 = chart.mark_line().encode(
+            x=alt.X("days_prior:Q").scale(reverse=True),
+            y=alt.Y("std_dev:Q", title="Std Dev Bid Price"),
+            color="leg_id:N",
+        )
+        if "source" in df.columns:
+            fig1 = fig1.encode(strokeDash="source:N", strokeWidth="source:N")
+        fig = fig | fig1
 
     return fig

614da7ca16cf544c086a902f9acc7e7e9616a32b
Jeff Newman <jeff@driftless.xyz>
2025-02-21 15:49:48 -0600

    pax.from_file

--- passengersim/__init__.py
+++ passengersim/__init__.py
@@ -62,3 +62,30 @@ def logging(level=None):
     from ._logging import log_to_console
 
     log_to_console(level)
+
+
+def _has_file_extensions(path: str, ext: list[str]) -> bool:
+    path = str(path)
+    lowercase_path = path.lower()
+    return any(lowercase_path.endswith(e) for e in ext)
+
+def from_file(path: str):
+    """Load a PassengerSim file.
+
+    The type of file is determined by the extension.  The following file types
+    are supported:
+
+    - `.pxsim`: SimulationTables
+    - `.yaml`, `.yml`, or either also with `.gz` or `.lz4`: Config
+
+    Parameters
+    ----------
+    path : str
+        Path to the file to load. The file type is determined by the extension.
+    """
+    if _has_file_extensions(path, [".pxsim"]):
+        return SimulationTables.from_file(path)
+    elif _has_file_extensions(path, [".yaml", ".yml", ".yml.gz", ".yaml.gz", ".yml.lz4", ".yaml.lz4"]):
+        return Config.from_yaml(path)
+    else:
+        raise ValueError("Unknown file type")
